3419a115a1e6ab9ca0d42c3926540c8ac143817f
jonvegards <jonvegards@gmail.com>
2015-08-23 21:46:47 +0200

    okei
    jepp

--- heihei.cpp
+++ heihei.cpp
@@ -1,1 +1,16 @@
 // Testfil i .cpp-stil for å sjå om giten er i form
+
+// A comment line begins like this in C++ programs
+// Standard ANSI-C++ include files
+// using namespace std;
+#include <iostream>  // input and output
+#include <math.h>   // sine function
+int main (int argc, char* argv[])
+{
+  // convert the text argv[1] to double using atof:
+  double r = atof(argv[1]);
+  double s = sin(r);
+  std::cout << "Hei, verd! Har du det bra? sin(" << r << ")=" << s << '\n';
+  // success
+  return 0;
+}
