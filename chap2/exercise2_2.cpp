d39432b3af93d0d3faef232db4dffcb472a36515
jonvegards <jonvegards@gmail.com>
2015-08-25 18:55:12 +0200

    Modifisert oppg 2.2 til å snart inneholde plotting av diff mellom opp- og ned-sum

--- chap2/exercise2_2.cpp
+++ chap2/exercise2_2.cpp
@@ -6,23 +6,34 @@
 using namespace std;
 int main (int argc, char* argv[])
 {
-  	double N, n, i, j, sum1, sum2;        /* declare variables 64 bits*/
+  	double N, i, j, k, sum1, sum2;        /* declare variables 64 bits*/
   	//N = atof(argv[1]);  /* convert the text argv[1] to double */
-  	N = pow(10,10);
-	n = N;
-  	// Sum up
-  	for (i=1; i<=N; i++)
-  	{
-  		sum1 += 1 / i;
-  		sum2 += 1/n; // Sum down
-  		n -= 1;
-  	}
-  	printf("Sum up: %2.8f" ,sum1); /* Print result */
-    cout << endl; 
-  	printf("Sum up: %2.8f" ,sum2); /* Print result */
-  	cout << endl;
+    double hurra[10], hurrb[10];
+    int n, Q=5;
+  	for (n=0; n<=Q; n++){
+      N = pow(10,n);
+      k = N;
+      for (i=1; i<=N; i++){
+        sum1 += 1/i;    // Sum up
+        sum2 += 1/k;    // Sum down
+        k -= 1;
+      }
+      hurra[n] = sum1;
+      hurrb[n] = sum2;
+    }
+  	//printf("Sum up: %2.8f" ,sum1); /* Print result */
+    //cout << endl; 
+  	//printf("Sum up: %2.8f" ,sum2); /* Print result */
+  	//cout << endl;
+    double diff[10];
+    for (n=0; n<=Q; n++){
+      //cout << hurra[n] << endl;
+      //cout << hurrb[n] << endl;
+      // Calculating the logarithm of the difference
+      diff[n] = log( (hurra[n]-hurrb[n])/hurrb[n] );
+      cout << diff[n] << endl;
+    }
     double final = sum2 - sum1;
-    cout << final;
-    cout << endl;
+    cout << final << endl;
   	return 0;           /* success execution of the program */
 }
