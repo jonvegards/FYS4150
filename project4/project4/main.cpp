86fbb5ba2112f28d1fd3e0af3050cc10f53cccb2
jonvegards <jonvegards@gmail.com>
2015-10-31 13:10:07 +0100

    parallellized code for project4

--- project4/project4/main.cpp
+++ project4/project4/main.cpp
@@ -6,8 +6,12 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <string>
 #include "lib.h"
 #include "time.h"
+#include "mpi.h"
+
+// /usr/local/bin/mpic++
 
 using namespace std;
 
@@ -29,7 +33,7 @@ using namespace std;
 // ensuring boundary conditions by
 // "coupling togheter" the elements
 // on the edges
-int periodic(int i, int dimension, int add) {
+inline int periodic(int i, int dimension, int add) {
     return (i+dimension+add) % (dimension);
     // The add-variable is either 1 or -1 depending on which side
     // of the element we are on, e.g.
@@ -37,102 +41,228 @@ int periodic(int i, int dimension, int add) {
     // the element below the one we're looking at
 }
 
-void metropolis_algo(int **state_matrix, double L_spins, double &energy, double &magnetization, double *w, long &idum);
-void SaveResultsFromSeveralMCCycles(double *, double *, double *, double *, int no_MC, double *);
+void metropolis_algo(int **state_matrix, int L_spins, double &energy, double &magnetization, double *w, long &idum, int &accepted, int **neighbour);
+void SaveResultsFromSeveralMCCycles(string FileName, double *HeatCapacity, double *Susceptibility, double *MeanMagnetization, double *MC_cycles, int N, double *, double *, double *);
 void CreateStartingState(int **, double, double , double , long &, bool);
+void ProbabilityForGivenEnergy(int **state_matrix, int L_spins, double &energy, int **neighbour);
+void SavingResultsForDifferentTemperatures(string FileName, double *e_var, double *m_var, double *mean_mag,
+                                           double *temperatures, int no_of_steps);
+void ExpectationValues(double *values, double normalization, double &accepted_total, double &HeatCapacity, double &Susceptibility, double &MeanMagnetization, int L);
+
+void RunForDifferentMCCycles();
+void RunForDifferentTemperatures(int argc, char *argv[]);
 
 
-int main(){
-    int**state_matrix;
-    double L_spins;
-    long idum;
-    double w[17]; // Array containing all possible energies for the system kind of sth what
-    double energy=0, magnetization=0,initial_temperature=0;
-    double values[5]; // Array for keeping our results
-
-    // Initial values
-    //double MC_cycles           = 10000;
-    int no_MC = 9;
-    double MC_cycles[no_MC];
-    double e_variance_aka_heat_capacity[no_MC];
-    double m_variance_aka_susceptibility[no_MC];
-    double mean_magnetization[no_MC];
-    MC_cycles[0] = 10;
-    for(int i=1; i<no_MC;i++) MC_cycles[i] = MC_cycles[i-1]*5;
-
-    L_spins             = 20.;
-    initial_temperature = 1.0;
-    idum                = -1;
-
-    // Initialising w
-    for(int delta_E=-8;delta_E<=8;delta_E++) w[delta_E+8] = exp(-delta_E/initial_temperature);
-
-    // Allocating memory for state matrix
-    state_matrix = (int **) matrix(L_spins, L_spins, sizeof(L_spins));
-    // Filling state matrix with either a random state or with every spin up
-    bool random = true;
-    CreateStartingState(state_matrix, L_spins, energy, magnetization, idum, random);
-
-    clock_t start, finish;
-    double time_used[no_MC];
-
-    for(int q=0;q<no_MC;q++){
-        start = clock();
-        // Setting values to zero
-        for(int i=0;i<5;i++) values[i] = 0;
-        // Using metropolis algorithm in order to reach an equilibrium
-        for(int n=0;n<MC_cycles[q]; n++){
-            metropolis_algo(state_matrix, L_spins, energy, magnetization, w, idum);
-            values[0] += energy;
-            values[1] += energy*energy;
-            values[2] += magnetization;
-            values[3] += magnetization*magnetization;
-            values[4] += fabs(magnetization);
-        }
-        finish = clock();
-
-        time_used[q] = ((double) (finish-start)/CLOCKS_PER_SEC);
-
-        // Print results oui oui
-        double normalization = MC_cycles[q];
-        e_variance_aka_heat_capacity[q] = (values[1]/normalization - values[0]*values[0]/(normalization*normalization))/(L_spins*L_spins);
-        m_variance_aka_susceptibility[q] = (values[3]/normalization - values[2]*values[2]/(normalization*normalization))/(L_spins*L_spins);
-        mean_magnetization[q] = values[4]/(normalization*L_spins*L_spins);
-    }
-    SaveResultsFromSeveralMCCycles(e_variance_aka_heat_capacity,m_variance_aka_susceptibility, mean_magnetization ,MC_cycles, no_MC, time_used);
-    free_matrix(((void **) state_matrix)); // free memory
+int main(int argc, char* argv[]){
+	//RunForDifferentMCCycles();
+	RunForDifferentTemperatures(argc, argv);
     return 0;
 }
-
-void metropolis_algo(int **state_matrix, double L_spins, double &energy, double &magnetization, double *w, long &idum){
-    for(int i=0;i<L_spins*L_spins;i++){
-        // Picking random site in lattice
-        int MC_row =    (int) (ran1(&idum)*L_spins);
-        int MC_column = (int) (ran1(&idum)*L_spins);
-        // calculating energy difference if one spin is flipped
-        int delta_E = 2*state_matrix[MC_row][MC_column]*
-                (state_matrix[MC_row][periodic(MC_column, L_spins, -1)] +
-                state_matrix[periodic(MC_row, L_spins, -1)][MC_column] +
-                state_matrix[MC_row][periodic(MC_column, L_spins, 1)] +
-                state_matrix[periodic(MC_row, L_spins, 1)][MC_column]);
-        // comparing random number with energy difference
-        if( delta_E <= 0 or ran1(&idum) <= w[delta_E + 8]){
-            state_matrix[MC_row][MC_column] *= -1;
-            energy += (double) delta_E;
-            magnetization += (double) 2*state_matrix[MC_row][MC_column];
+void metropolis_algo(int **state_matrix, int L_spins, double &energy, double &magnetization, double *w, long &idum, int &accepted, int **neighbour){
+    for(int row=0;row<L_spins;row++){
+        for(int column=0;column<L_spins;column++){
+            // Picking random site in lattice
+            int MC_row =    (int) (ran1(&idum)*(double)L_spins);
+            int MC_column = (int) (ran1(&idum)*(double)L_spins);
+            // calculating energy difference if one spin is flipped
+            int delta_E = 2*state_matrix[MC_row][MC_column]*
+                    (state_matrix[MC_row][neighbour[MC_column][0]] +
+                    state_matrix[neighbour[MC_row][0]][MC_column] +
+                    state_matrix[MC_row][neighbour[MC_column][1]] +
+                    state_matrix[neighbour[MC_row][1]][MC_column]);
+            // comparing random number with energy difference
+            if( delta_E <= 0 or ran1(&idum) <= w[delta_E + 8]){
+                accepted += 1;
+                state_matrix[MC_row][MC_column] *= -1;
+                energy += (double) delta_E;
+                magnetization += (double) 2*state_matrix[MC_row][MC_column];
+            }
         }
     }
 }
 
+void RunForDifferentMCCycles() {
+	//------------------------------------------------------
+	// Main parameters
+	// No. of MC-values we calculate for
+	int MC_cycles = 1000000;
+	int M = 10000; // We collect data for each cycle of M
+	int N = MC_cycles/M;
+	double start_temperature = 2.4;
+	int L = 20; // Size of lattice
+	bool random = false; // Starting with a random or non-random state
+	string FileName = "RunningWithSeveralMCOrderedStartT24_blabla.m";
+	//------------------------------------------------------
+    double values[5]; // Array for keeping our results
+    double energy=0, magnetization=0;
+    double w[17]; // Array containing all possible energies for the system kind of sth what
+    long idum  = -1; // MC-seed
+
+    // Creating array which contain information about the neighbouring
+    // spins when we use periodic boundary conditions.
+    int **neighbour;
+    neighbour = (int **) matrix(L,2,sizeof(L));
+    for(int i=0;i<L;i++){
+        neighbour[i][0] = periodic(i,L,-1);
+        neighbour[i][1] = periodic(i,L,1);
+    }
+
+
+	double MC_cycles_no[N];
+	MC_cycles_no[0] = M;
+	for(int i=1; i<N;i++) MC_cycles_no[i] = M + i*M;
+
+    // We need 10^5 MC-cycles for reaching equilibrium from a random start state when T=2
+
+    double HeatCapacity[N];
+    double Susceptibility[N];
+    double MeanMagnetization[N];
+    double probability[N];
+    double time_used[N], accepted_total[N];
+    int accepted;
+
+    // Initialising w
+    for(int delta_E=-8;delta_E<=8;delta_E++) w[delta_E+8] = exp(-delta_E/start_temperature);
+	for(int i=0;i<N;i++) accepted_total[i]=0, probability[i]=0;
+
+	// Setting expectation values to zero
+	for(int i=0;i<5;i++) values[i] = 0;
+	clock_t start, finish;
+	int**state_matrix;
+	// Allocating memory for state matrix and then filling it
+	state_matrix = (int **) matrix(L, L, sizeof(L));
+	CreateStartingState(state_matrix, L, energy, magnetization, idum, random);
+    // loop over different number of MC-cycles
+	// Using metropolis algorithm in order to reach an equilibrium
+	start = clock();
+	for(int q=0;q<N;q++){
+		for(int n=0;n<=M; n++){
+			accepted = 0;
+			metropolis_algo(state_matrix, L, energy, magnetization, w, idum, accepted, neighbour);
+			values[0] += energy;
+			values[1] += energy*energy;
+			values[2] += magnetization;
+			values[3] += magnetization*magnetization;
+			values[4] += fabs(magnetization);
+			accepted_total[q] += accepted;
+		}
+		cout << MC_cycles_no[q] << endl;
+		ExpectationValues(values, MC_cycles_no[q], accepted_total[q], HeatCapacity[q], Susceptibility[q], MeanMagnetization[q], L);
+		// Calculate probabilities for having a certain state after reaching equilibrium
+		if( MC_cycles_no[q] > 40000 ) ProbabilityForGivenEnergy(state_matrix, L, probability[q], neighbour);
+		finish = clock();
+		time_used[q] = ((double) (finish-start)/CLOCKS_PER_SEC);
+
+	}
+	free_matrix(((void **) state_matrix)); // free memory
+
+
+	SaveResultsFromSeveralMCCycles(FileName, HeatCapacity,Susceptibility, MeanMagnetization, MC_cycles_no, N, time_used, accepted_total, probability);
+}
+
+void RunForDifferentTemperatures(int argc, char* argv[]) {
+	//------------------------------------------------------
+	// Main parameters
+	// No. of MC-values we calculate for
+	double start_temp = 2.0;
+	double final_temp = 2.6;
+	double temp_step  = 0.05;
+	double MC = 1000000;
+	int N = (final_temp - start_temp)/temp_step + 1;
+	int L = 40; // Size of lattice 40,60,80
+	bool random = true; // Starting with a random or non-random state
+	string FileName = "L_60_MC_1000000_DifferentTemps_parallell.m";
+	//------------------------------------------------------
+	double values[5], total_values[5]; // Array for keeping our results
+    double energy=0, magnetization=0;
+    double w[17]; // Array containing all possible energies for the system kind of sth what
+
+	// Creating array which contain information about the neighbouring
+	// spins when we use periodic boundary conditions.
+	int **neighbour;
+	neighbour = (int **) matrix(L,2,sizeof(L));
+	for(int i=0;i<L;i++){
+		neighbour[i][0] = periodic(i,L,-1);
+		neighbour[i][1] = periodic(i,L,1);
+	}
+
+	double HeatCapacity[N];
+	double Susceptibility[N];
+	double MeanMagnetization[N];
+	double temperature[N];
+	double accepted_total[N];
+	for(int i=0;i<N;i++) temperature[i] = start_temp + i*temp_step;
+	// Allocating memory for state matrix and then filling it
+	int**state_matrix;
+	//------------------------------------------------------
+	// Trying out MPI
+	//  MPI initializations
+	int my_rank, numprocs;
+	MPI_Init (&argc, &argv);
+	MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+	MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+	int no_intervalls = MC/numprocs;
+	int myloop_begin = my_rank*no_intervalls + 1;
+	int myloop_end = (my_rank+1)*no_intervalls;
+	if ( (my_rank == numprocs-1) && ( myloop_end <= MC) ) myloop_end = MC;
+
+	// broadcast to all nodes common variables
+	MPI_Bcast (&L, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast (&start_temp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast (&final_temp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast (&temp_step, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	long idum = -1 - my_rank;
+
+    state_matrix = (int **) matrix(L, L, sizeof(L));
+	CreateStartingState(state_matrix, L, energy=0, magnetization=0, idum, random);
+
+	for ( int q=0;q<N;q++){
+        // Setting values to zero
+        //temperature+=temp_step;
+		for(int i=0;i<5;i++) values[i] = 0, total_values[i] = 0;
+        for(int delta_E=-8;delta_E<=8;delta_E++) w[delta_E+8] = exp(-delta_E/temperature[q]);
+
+		//for(int n=0;n<MC; n++){
+		for (int cycles = myloop_begin; cycles <= myloop_end; cycles++){
+			int accepted = 0;
+			metropolis_algo(state_matrix, L, energy, magnetization, w, idum, accepted, neighbour);
+			values[0] += energy;
+			values[1] += energy*energy;
+			values[2] += magnetization;
+			values[3] += magnetization*magnetization;
+			values[4] += fabs(magnetization);
+			accepted_total[q] += accepted;
+		}
+		//cout << q << endl;
+		//ExpectationValues(values, MC, accepted_total[q], HeatCapacity[q], Susceptibility[q], MeanMagnetization[q], L);
+
+		// Find total average
+		for( int i =0; i < 5; i++){
+		  MPI_Reduce(&values[i], &total_values[i], 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+		}
+		// print results
+		if ( my_rank == 0) {
+			cout << "Nå har jeg kommet til: " << temperature[q] << endl;
+			ExpectationValues(total_values, MC, accepted_total[q], HeatCapacity[q], Susceptibility[q], MeanMagnetization[q], L);;
+		}
+    }
+	free_matrix(((void **) state_matrix)); // free memory
+	// End MPI
+	MPI_Finalize ();
+
+	if ( my_rank == 0) {
+		SavingResultsForDifferentTemperatures(FileName, HeatCapacity, Susceptibility, MeanMagnetization, temperature, N);
+	  }
+}
 
 void CreateStartingState(int **state_matrix, double L_spins, double energy, double magnetization, long &idum, bool random){
     if(random){
-        cout << "random" << endl;
         // Filling state matrix with random spins
         for(int row=0;row<L_spins;row++){
             for(int column=0;column<L_spins;column++){
                 if(ran1(&idum) <= 0.5) state_matrix[row][column] = 1.;
-
+                else state_matrix[row][column] = -1;
             }
         }
     }
@@ -157,40 +287,106 @@ void CreateStartingState(int **state_matrix, double L_spins, double energy, doub
 
 }
 
-void SaveResultsFromSeveralMCCycles(double *e_var, double *m_var, double *mean_mag, double *MC, int no_MC, double *time_used){
+void SaveResultsFromSeveralMCCycles(string FileName, double *HeatCapacity, double *Susceptibility, double *MeanMagnetization, double *MC_cycles,
+                                    int N, double *time_used, double *accepted_total,
+                                    double *probability){
     ofstream myfile;
-    myfile.open("RunningWithSeveralMC_random_start.m");
+	myfile.open(FileName);
     myfile << "MC " << "= [";
-    for (int i=0; i<no_MC; i++){
-        myfile << MC[i] << ", ";
+    for (int i=0; i<N; i++){
+        myfile << MC_cycles[i] << ", ";
     }
     myfile << "];" << endl;
 
     myfile << "e_var " << "= [";
-    for (int i=0; i<no_MC; i++){
+    for (int i=0; i<N; i++){
+        myfile << HeatCapacity[i] << ", ";
+    }
+    myfile << "];" << endl;
+
+    myfile << "m_var " << "= [";
+    for (int i=0; i<N; i++){
+        myfile << Susceptibility[i] << ", ";
+    }
+    myfile << "];" << endl;
+
+    myfile << "mean_mag " << "= [";
+    for (int i=0; i<N; i++){
+        myfile << MeanMagnetization[i] << ", ";
+    }
+    myfile << "];" << endl;
+
+    myfile << "time_used " << "= [";
+    for (int i=0; i<N; i++){
+        myfile << time_used[i] << ", ";
+    }
+    myfile << "];" << endl;
+    myfile << "accepted_states " << "= [";
+    for (int i=0; i<N; i++){
+        myfile << accepted_total[i] << ", ";
+    }
+    myfile << "];" << endl;
+
+    myfile << "probability " << "= [";
+    for (int i=0; i<N; i++){
+        myfile << probability[i] << ", ";
+    }
+    myfile << "];" << endl;
+
+	myfile << "plot(MC, e_var)" << endl;
+
+    myfile.close();
+}
+
+void ProbabilityForGivenEnergy(int **state_matrix, int L_spins, double &energy, int **neighbour){
+    // Function for calculating the probability of a given energy
+	energy = 0;
+    for(int row=0;row<L_spins;row++){
+        for(int column=0;column<L_spins;column++){
+			energy -= (double) state_matrix[row][column]*(state_matrix[neighbour[row][0]][column] +
+													state_matrix[row][neighbour[column][0]]);
+        }
+    }
+	//energy= energy/((double)L_spins*(double)L_spins);
+}
+
+void SavingResultsForDifferentTemperatures(string FileName, double *e_var, double *m_var, double *mean_mag,
+                                           double *temperatures, int no_of_steps){
+    ofstream myfile;
+    myfile.open (FileName);
+    myfile << "e_var " << "= [";
+    for (int i=0; i<no_of_steps; i++){
         myfile << e_var[i] << ", ";
     }
     myfile << "];" << endl;
 
     myfile << "m_var " << "= [";
-    for (int i=0; i<no_MC; i++){
+    for (int i=0; i<no_of_steps; i++){
         myfile << m_var[i] << ", ";
     }
     myfile << "];" << endl;
 
     myfile << "mean_mag " << "= [";
-    for (int i=0; i<no_MC; i++){
+    for (int i=0; i<no_of_steps; i++){
         myfile << mean_mag[i] << ", ";
     }
     myfile << "];" << endl;
 
-    myfile << "time_used " << "= [";
-    for (int i=0; i<no_MC; i++){
-        myfile << time_used[i] << ", ";
+    myfile << "temperatures " << "= [";
+    for (int i=0; i<no_of_steps; i++){
+        myfile << temperatures[i] << ", ";
     }
     myfile << "];" << endl;
 
-    myfile << "semilogx(MC, e_var)" << endl;
+	myfile << "test = e_var./temperatures;" << endl;
+	myfile << "plot(temperatures,test)" << endl;
 
     myfile.close();
+}
+
+void ExpectationValues(double *values, double normalization, double &accepted_total, double &HeatCapacity, double &Susceptibility, double &MeanMagnetization, int L){
+	accepted_total = accepted_total/normalization;
+	HeatCapacity = (values[1]/normalization - values[0]*values[0]/(normalization*normalization)) / (L*L);
+	Susceptibility = (values[3]/normalization - values[4]*values[4]/(normalization*normalization))/(L*L);
+    MeanMagnetization = values[4]/(normalization*L*L);
 }
--- project4/project4/project4.pro
+++ project4/project4/project4.pro
@@ -1,5 +1,5 @@
 TEMPLATE = app
-CONFIG += console c++11
+CONFIG += console c++11 -O3
 CONFIG -= app_bundle
 CONFIG -= qt
 
@@ -9,3 +9,14 @@ SOURCES += main.cpp \
 HEADERS += \
     lib.h
 
+# MPI Settings
+QMAKE_CXX = mpic++
+QMAKE_CXX_RELEASE = $$QMAKE_CXX
+QMAKE_CXX_DEBUG = $$QMAKE_CXX
+QMAKE_LINK = $$QMAKE_CXX
+QMAKE_CC = mpicc
+
+QMAKE_CFLAGS += $$system(/usr/local/bin/mpic++ --showme:compile)
+QMAKE_LFLAGS += $$system(/usr/local/bin/mpic++ --showme:link)
+QMAKE_CXXFLAGS += $$system(/usr/local/bin/mpic++ --showme:compile) -DMPICH_IGNORE_CXX_SEEK
+QMAKE_CXXFLAGS_RELEASE += $$system(/usr/local/bin/mpic++ --showme:compile) -DMPICH_IGNORE_CXX_SEEK
